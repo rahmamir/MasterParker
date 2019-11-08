6039d9c9b926c702c81e651ba8fdeaf96230a67c
syedrehan <faaizrehan@hotmail.com>
2019-11-08 02:32:53 -0500

    Sign In VC Class Added with proper constraints

---
+++ MasterParkerV1/view/SignUpVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpVC.swift
+//  MasterParkerV1
+//
+//  Created by Syed Rehan on 2019-11-08.
+//  Copyright © 2019 MR. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
