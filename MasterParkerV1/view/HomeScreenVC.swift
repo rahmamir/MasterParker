22099c7861f701534cd46e5ca66a753fc210051b
MirzaRahman <rahmamir@sheridancollege.ca>
2019-11-15 09:01:12 -0500

    implemented basic home screen with only one functioning button (AddParking)

---
+++ MasterParkerV1/view/HomeScreenVC.swift
@@ -0,0 +1,36 @@
+//
+//  HomeScreenVC.swift
+//  MasterParkerV1
+//
+//  Created by Mirza Rahman on 2019-11-15.
+//  Copyright © 2019 MR. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC : UIViewController {
+    
+    @IBOutlet var AddParkingBtn : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func onAddParkingClick(_ sender: UIButton){
+        
+        self.navigateTo()
+        
+    }
+    
+    private func navigateTo(){//TODO = dynamically navigate to appropriate page
+        
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let addParkingVC = storyBoard.instantiateViewController(withIdentifier: "AddParkingScene") as! AddParkingVC
+        navigationController?.pushViewController(addParkingVC, animated: true)
+        
+    }
+    
+}
